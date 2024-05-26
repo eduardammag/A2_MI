1e581f3fb3dd1b3370cdecd56072187f97b7bb61
Vitor do Nascimento <45907317+vitor-n@users.noreply.github.com>
2024-05-26 17:58:37 -0300

    add DW_DLL.sql

---
+++ DW_DDL.sql
@@ -0,0 +1,92 @@
+drop schema if exists dw_eal cascade;
+create schema dw_eal;
+
+set search_path=dw_eal;
+
+CREATE TABLE CALENDARIO (
+    CalendarioKey INT NOT NULL,
+    DataCompleta TIMESTAMP NOT NULL,
+    DiaDaSemana VARCHAR(10) NOT NULL,
+    DiaDoMes INT NOT NULL,
+    Mes INT NOT NULL,
+    Trimestre INT NOT NULL,
+    Ano INT NOT NULL,
+    PRIMARY KEY (CalendarioKey)
+);
+
+CREATE TABLE CLIENTE (
+    ClienteKey INT NOT NULL,
+    ClienteID INT NOT NULL,
+    ClienteNome VARCHAR(255) NOT NULL,
+    DataNascimento DATE NOT NULL,
+    PRIMARY KEY (ClienteKey)
+);
+
+CREATE TABLE CLIENTE_ENDERECO (
+    EnderecoKey INT NOT NULL,
+    ClienteID INT NOT NULL,
+    Logradouro VARCHAR(255) NOT NULL,
+    Municipio VARCHAR(255) NOT NULL,
+    Bairro VARCHAR(255) NOT NULL,
+    Estado CHAR(2) NOT NULL,
+    PRIMARY KEY (EnderecoKey)
+);
+
+CREATE TABLE SERVICO_TRANSACAO (
+    TransacaoKey INT NOT NULL,
+    ClienteID INT NOT NULL,
+    ServicoID INT NOT NULL,
+    TransacaoData TIMESTAMP NOT NULL,
+    ServicoTipo VARCHAR(255) NOT NULL,
+    ServicoValor MONEY NOT NULL,
+    Quantidade INT NOT NULL,
+    PRIMARY KEY (TransacaoKey)
+);
+
+
+CREATE TABLE FUNCIONARIO (
+    FuncKey INT NOT NULL,
+    FuncID INT NOT NULL,
+    Cargo VARCHAR(255) NOT NULL,
+    FuncNome VARCHAR(255) NOT NULL,
+    PRIMARY KEY (FuncKey)
+);
+
+CREATE TABLE PAGAMENTO (
+    PagtoKey INT NOT NULL,
+    PagtoID INT NOT NULL,
+    PagtoValor MONEY NOT NULL,
+    PagtoData DATE NOT NULL,
+    FuncID INT NOT NULL,
+    PRIMARY KEY (PagtoKey)
+);
+
+
+-- Fact tables
+
+CREATE TABLE DESPESAS (
+    DespesaID INT NOT NULL,
+    FuncKey INT NOT NULL,
+    PagtoKey INT NOT NULL,
+    CalendarioKey INT NOT NULL,
+    ValorGasto MONEY NOT NULL,
+    PRIMARY KEY (DespesaID),
+    FOREIGN KEY (FuncKey) REFERENCES FUNCIONARIO(FuncKey),
+    FOREIGN KEY (PagtoKey) REFERENCES PAGAMENTO(PagtoKey),
+    FOREIGN KEY (CalendarioKey) REFERENCES CALENDARIO(CalendarioKey)
+);
+
+CREATE TABLE RECEITA (
+    ReceitaID INT NOT NULL,
+    CalendarioKey INT,
+    ClienteKey INT,
+    EnderecoKey INT,
+    TransacaoKey INT,
+    ValorRecebido DECIMAL(10, 2),
+    Hora TIME,
+    PRIMARY KEY (ReceitaID),
+    FOREIGN KEY (CalendarioKey) REFERENCES CALENDARIO(CalendarioKey),
+    FOREIGN KEY (ClienteKey) REFERENCES CLIENTE(ClienteKey),
+    FOREIGN KEY (EnderecoKey) REFERENCES CLIENTE_ENDERECO(EnderecoKey),
+    FOREIGN KEY (TransacaoKey) REFERENCES SERVICO_CONTRATADO(TransacaoKey)
+);
